2a30a8ec686e8e8b6d78b555e364d5e90088cfc6
davidrt <jdramirezt@unal.edu.co>
2024-02-01 13:18:17 -0500

    local route

--- client/src/api/users.api.js
+++ client/src/api/users.api.js
@@ -1,4 +1,5 @@
 import axios from `axios`;
 var renderServer = 'https://coffeserver.onrender.com'
+var localHost = 'http://localhost:4000'
 export const autenticateUserRequest = async (userName, pass) =>
     await axios.get(`${renderServer}/users/${userName}/${pass}`);
