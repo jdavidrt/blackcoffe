8364ac16b45ddf7f13e4c280f835bf87d1dd4fad
davidrt <jdramirezt@unal.edu.co>
2024-02-16 12:26:11 -0500

    auth fix 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,9 +24,11 @@ function App() {
   const navigate = useNavigate();
   //console.log('app', localStorage.getItem('user'));
   useEffect(() => {
-    if (!window.location.pathname.includes("/iniciarSesion") && localStorage.getItem('user') === "") (
+    if (!window.location.pathname.includes("/iniciarSesion") && localStorage.getItem('user') === "") {
       navigate("/iniciarSesion")
-    )
+    } else if (localStorage.getItem('user') === "") {
+      navigate("/iniciarSesion")
+    }
   }, [window.location.pathname]);
   return (
     <div className="bg-slate-200 h-screen">
